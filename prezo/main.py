3d661c3ac0d6969f524ceca116aae0fda26c64b0
Vsevolod Kartashev <h1bymask@gmail.com>
2022-03-18 22:53:14 +0300

    Brython

---
+++ prezo/main.py
@@ -0,0 +1,172 @@
+slide("лого_персиковый.svg")
+read_key()
+
+slide("slides/нейтральный1.svg")
+play("speech/можете забрать завтрак, приятного аппетита.wav")
+articulation(82, "articulation/нейтрзакр.svg")
+articulation(14, "articulation/нейтрзакр-нейрполуоткр.svg")
+articulation(68, "articulation/нейтральный1(полуоткрытый_рот).svg")
+articulation(14, "articulation/нейтрполуоткр-злойоткр.svg")
+articulation(27, "articulation/нейтральный2(открытый_рот).svg")
+articulation(14, "articulation/злойоткр-нейрполуоткр.svg")
+articulation(27, "articulation/нейтральный1(полуоткрытый_рот).svg")
+articulation(14, "articulation/нейтр-полуоткрытый-закрытый.svg")
+articulation(27, "articulation/нейтрзакр.svg")
+articulation(82, "articulation/нейтрзакр.svg")
+articulation(14, "articulation/нейтрзакр-нейрполуоткр.svg")
+articulation(68, "articulation/нейтральный1(полуоткрытый_рот).svg")
+articulation(14, "articulation/нейтрполуоткр-злойоткр.svg")
+articulation(27, "articulation/нейтральный2(открытый_рот).svg")
+articulation(14, "articulation/злойоткр-нейрполуоткр.svg")
+articulation(27, "articulation/нейтральный1(полуоткрытый_рот).svg")
+articulation(14, "articulation/нейтр-полуоткрытый-закрытый.svg")
+articulation(27, "articulation/нейтрзакр.svg")
+articulation(82, "articulation/нейтрзакр.svg")
+articulation(14, "articulation/нейтрзакр-нейрполуоткр.svg")
+articulation(68, "articulation/нейтральный1(полуоткрытый_рот).svg")
+articulation(14, "articulation/нейтрполуоткр-злойоткр.svg")
+articulation(27, "articulation/нейтральный2(открытый_рот).svg")
+articulation(14, "articulation/злойоткр-нейрполуоткр.svg")
+articulation(27, "articulation/нейтральный1(полуоткрытый_рот).svg")
+articulation(14, "articulation/нейтр-полуоткрытый-закрытый.svg")
+articulation(27, "articulation/нейтрзакр.svg")
+
+read_key()
+
+slide("slides/слайд1.svg")
+read_key()
+
+slide("slides/нейтральный1.svg")
+play("speech/выберите, что вам нужно сделать.wav")
+articulation(500, "articulation/нейтрзакр-нейтрполуоткр.svg")
+articulation(500, "articulation/нейтр-полуоткрытый-закрытый.svg")
+ 
+read_key()
+
+articulation(0, "пусто.svg")
+slide("slides/слайд3.svg")
+ 
+read_key()
+
+slide("slides/слайд5.svg")
+play("speech/приложите палец.wav")
+wait_audio()
+ 
+read_key()
+
+slide("slides/злой.svg")
+play("speech/можете забрать лекарства.wav")
+articulation(500, "articulation/нейтрзакр-нейтрполуоткр.svg")
+articulation(500, "articulation/нейтр-полуоткрытый-закрытый.svg")
+
+read_key()
+
+articulation(0, "пусто.svg")
+slide("slides/слайд7.svg")
+	
+slide("slides/злой.svg")
+play("speech/выберите, что вы хотите сделать.wav")
+articulation(500, "articulation/нейтрзакр-нейтрполуоткр.svg")
+articulation(500, "articulation/нейтр-полуоткрытый-закрытый.svg")
+ 
+articulation(0, "пусто.svg")
+slide("slides/слайд9.svg")
+ 
+read_key()
+
+slide("slides/нейтральный1.svg")
+play("speech/можете забрать завтрак, приятного аппетита.wav")
+articulation(500, "articulation/нейтрзакр-нейтрполуоткр.svg")
+articulation(500, "articulation/нейтр-полуоткрытый-закрытый.svg")
+
+articulation(0, "пусто.svg")
+slide("slides/слайд11.svg")
+ 
+read_key()
+
+slide("slides/грустный.svg")
+play("speech/выберите, что вы хотите сделать.wav")
+articulation(500, "articulation/нейтрзакр-нейтрполуоткр.svg")
+articulation(500, "articulation/нейтр-полуоткрытый-закрытый.svg")
+
+articulation(0, "пусто.svg")
+slide("slides/слайд13.svg")
+ 
+read_key()
+
+slide("slides/злой.svg")
+play("speech/выберите, что вам нужно сделать.wav")
+articulation(500, "articulation/нейтрзакр-нейтрполуоткр.svg")
+articulation(500, "articulation/нейтр-полуоткрытый-закрытый.svg")
+ 
+articulation(0, "пусто.svg")
+slide("slides/слайд15.svg")
+
+read_key()
+
+slide("slides/нейтральный1.svg")
+play("speech/выберите, что вам нужно сделать.wav")
+articulation(500, "articulation/нейтрзакр-нейтрполуоткр.svg")
+articulation(500, "articulation/нейтр-полуоткрытый-закрытый.svg")
+
+read_key()
+
+slide("slides/грустный.svg")
+play("speech/media8.wav")
+articulation(500, "articulation/нейтрзакр-нейтрполуоткр.svg")
+articulation(500, "articulation/злой-открытый-закрытый.svg")
+
+articulation(0, "пусто.svg")
+slide("slides/слайд19.svg")
+
+read_key()
+
+slide("slides/нейтральный1.svg")
+play("speech/media10.wav")
+articulation(82, "articulation/нейтрзакр.svg")
+articulation(14, "articulation/нейтрзакр-нейрполуоткр.svg")
+articulation(68, "articulation/нейтральный1(полуоткрытый_рот).svg")
+articulation(14, "articulation/нейтрполуоткр-злойоткр.svg")
+articulation(27, "articulation/нейтральный2(открытый_рот).svg")
+articulation(14, "articulation/злойоткр-нейрполуоткр.svg")
+articulation(27, "articulation/нейтральный1(полуоткрытый_рот).svg")
+articulation(14, "articulation/нейтр-полуоткрытый-закрытый.svg")
+articulation(27, "articulation/нейтрзакр.svg")
+
+read_key()
+
+articulation(0, "пусто.svg")
+slide("slides/слайд21.svg")
+
+articulation(0, "пусто.svg")
+slide("slides/слайд22.svg")
+
+read_key()
+
+articulation(0, "пусто.svg")
+slide("slides/слайд23.svg")
+
+read_key()
+
+slide("slides/грустный.svg")
+play("speech/изменения сохранены.wav")
+articulation(500, "articulation/нейтрзакр-нейтрполуоткр.svg")
+articulation(500, "articulation/нейтр-полуоткрытый-закрытый.svg")
+
+articulation(0, "пусто.svg")
+slide("slides/слайд25.svg")
+
+read_key() 
+
+# articulation(500, "нейтрзакр.svg")
+# articulation(500, "злойоткр-нейрполуоткр.svg")
+# read_key()
+# articulation(500, "нейтрполуоткр-злойоткр.svg")
+# read_key()
+# articulation(500, "злой-открытый-закрытый.svg")
+# read_key()
+# articulation(500, "злойоткр-нейрполуоткр.svg")
+# read_key()
+# articulation(500, "нейтрзакр-нейтрполуоткр.svg")
+# read_key()
+# articulation(500, "нейтр-полуоткрытый-закрытый.svg")
