2d0e51c834ff2770f15bfef7f96fb3611f593511
klochaa <andreykl07@mail.ru>
2022-06-10 14:54:54 +0300

    Update pharmacy.py

--- prezo/pharmacy.py
+++ prezo/pharmacy.py
@@ -1,110 +1,27 @@
 from articulation import enter_face, speak_and_articulate
 
-
-slide("slides/слайд1.svg")
-
 await enter_face()
-await speak_and_articulate("выберите, ---что -вам -нужно -сделать")
-
-articulation(0, "пусто.svg")
-slide("slides/слайд3.svg")
-
-await enter_face()
-await speak_and_articulate("приложите -палец")
-
-articulation(0, "пусто.svg")
-slide("slides/слайд5.svg")
-
-await enter_face()
-await speak_and_articulate("можете -забрать -лекарства")
-
-await key()
-
-articulation(0, "пусто.svg")
-slide("slides/слайд7.svg")
-
-await enter_face()
-play("speech/выберите, что вы хотите сделать.wav")
-articulation(500, "articulation/нейтрзакр-нейтрполуоткр.svg")
-articulation(500, "articulation/нейтр-полуоткрытый-закрытый.svg")
+await speak_and_articulate("Добрый день! --Вас интересуют таблетки -или сироп?")
  
-articulation(0, "пусто.svg")
-slide("slides/слайд9.svg")
+await enter_face()
+await speak_and_articulate("В наличии остался только горький. --Сладкий закончился")
 
 
 await enter_face()
-play("speech/можете забрать завтрак, приятного аппетита.wav")
-articulation(500, "articulation/нейтрзакр-нейтрполуоткр.svg")
-articulation(500, "articulation/нейтр-полуоткрытый-закрытый.svg")
-
-articulation(0, "пусто.svg")
-slide("slides/слайд11.svg")
-
+await speak_and_articulate("Привезут только через неделю")
 
 await enter_face()
-play("speech/выберите, что вы хотите сделать.wav")
-articulation(500, "articulation/нейтрзакр-нейтрполуоткр.svg")
-articulation(500, "articulation/нейтр-полуоткрытый-закрытый.svg")
+speak_and_articulate("покупайте горький сироп. --А как иначе. --Вы же хотите выздороветь")
 
-articulation(0, "пусто.svg")
-slide("slides/слайд13.svg")
-
-
-await enter_face()
-play("speech/выберите, что вам нужно сделать.wav")
-articulation(500, "articulation/нейтрзакр-нейтрполуоткр.svg")
-articulation(500, "articulation/нейтр-полуоткрытый-закрытый.svg")
  
-articulation(0, "пусто.svg")
-slide("slides/слайд15.svg")
-
 await enter_face()
-play("speech/выберите, что вам нужно сделать.wav")
-articulation(500, "articulation/нейтрзакр-нейтрполуоткр.svg")
-articulation(500, "articulation/нейтр-полуоткрытый-закрытый.svg")
+speak_and_articulate("Есть Нормализин. --Выздоровеете -и глазом не моргнёте")
 
 
 await enter_face()
-play("speech/media8.wav")
-articulation(500, "articulation/нейтрзакр-нейтрполуоткр.svg")
-articulation(500, "articulation/злой-открытый-закрытый.svg")
-
-articulation(0, "пусто.svg")
-slide("slides/слайд19.svg")
+speak_and_articulate("Нет, -эти не стоит. --Может тогда Горлонеболин? --Его можно принимать, -если у Вас нет температуры")
 
 
 await enter_face()
-play("speech/media10.wav")
-articulation(82, "articulation/нейтрзакр.svg")
-articulation(14, "articulation/нейтрзакр-нейрполуоткр.svg")
-articulation(68, "articulation/нейтральный1(полуоткрытый_рот).svg")
-articulation(14, "articulation/нейтрполуоткр-злойоткр.svg")
-articulation(27, "articulation/нейтральный2(открытый_рот).svg")
-articulation(14, "articulation/злойоткр-нейрполуоткр.svg")
-articulation(27, "articulation/нейтральный1(полуоткрытый_рот).svg")
-articulation(14, "articulation/нейтр-полуоткрытый-закрытый.svg")
-articulation(27, "articulation/нейтрзакр.svg")
+speak_and_articulate("Принимайте его три раза в день -после еды. --Выздоравливайте!")
 
-await key()
-
-articulation(0, "пусто.svg")
-slide("slides/слайд21.svg")
-
-articulation(0, "пусто.svg")
-slide("slides/слайд22.svg")
-
-await key()
-
-articulation(0, "пусто.svg")
-slide("slides/слайд23.svg")
-
-
-await enter_face()
-play("speech/изменения сохранены.wav")
-articulation(500, "articulation/нейтрзакр-нейтрполуоткр.svg")
-articulation(500, "articulation/нейтр-полуоткрытый-закрытый.svg")
-
-articulation(0, "пусто.svg")
-slide("slides/слайд25.svg")
-
-await key() 
