ff5ccc838add4865b6c76a34f51ac8e9a39f7692
Vsevolod Kartashev <h1bymask@gmail.com>
2023-01-27 18:08:33 +0300

    pharmacy2; click_face

---
+++ prezo/pharmacy2.py
@@ -0,0 +1,27 @@
+from articulation import click_face, speak_and_articulate
+
+await click_face()
+await speak_and_articulate("--Добрый день! --Вас интересуют таблетки или сироп?")
+ 
+await click_face()
+await speak_and_articulate("В наличии остался только горький. ----Сладкий закончился")
+
+
+await click_face()
+await speak_and_articulate("Привезут только через неделю")
+
+await click_face()
+await speak_and_articulate("покупайте горький сироп. ---А как иначе. ----Вы же хотите выздороветь")
+
+ 
+await click_face()
+await speak_and_articulate("-Есть Нормализин. ---Выздоровеете -и глазом не моргнёте")
+
+
+await click_face()
+await speak_and_articulate("Нет, -эти не стоит. --Может тогда Горлонеболин? ----Его можно принимать, -если у Вас нет температуры")
+
+
+await click_face()
+await speak_and_articulate("Принимайте его три раза в день -после еды. --Выздоравливайте!")
+
