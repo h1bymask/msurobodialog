3468cb5b067f261d698414cee39ef554cb8efadc
klochaa <101871387+klochaa@users.noreply.github.com>
2023-03-13 21:17:06 +0300

    Update chat_A.py

--- prezo/chat_A.py
+++ prezo/chat_A.py
@@ -2,10 +2,10 @@ from articulation import click_face, speak_and_articulate, click_eyes
 
 
 await click_eyes("eyes/глаза_улыбающийся1.svg")
-await speak_and_articulate("привет, ---ты видел, --что -вчера -написали -в группу")
+await speak_and_articulate("привет, ---ты видел, --что -вчера -написали -в группу-")
 
 await click_eyes("eyes/глаза_улыбающийся1.svg")
-await speak_and_articulate("-зачета --завтра -не будет, ---его -перенесли --на субботу!")
+await speak_and_articulate("-зачета -завтра --не будет, ---его -перенесли --на субботу!")
  
 await click_eyes("eyes/глаза_злой.svg")
 await speak_and_articulate("-ты -всегда -так говоришь --и --сдаешь -на тройку, --может --пора начинать учить--")
