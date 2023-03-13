30bccda767e86d8e790cb7307a9e774fa0070d75
klochaa <101871387+klochaa@users.noreply.github.com>
2023-03-13 19:57:37 +0300

    Update chat_F.py

--- prezo/chat_F.py
+++ prezo/chat_F.py
@@ -14,4 +14,4 @@ await speak_and_articulate("-не --повышай -на меня голос!")
 
 await key("click")
 slide("eyes/глаза_улыбающийся1.svg")
-await speak_and_articulate("-пойдем --лучше -в столовую-, -я угощаю!")
+await speak_and_articulate("-пойдем --лучше --в столовую, -я угощаю!")
