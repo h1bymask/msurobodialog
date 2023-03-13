3f0a724f7b5ff890ea08334b0782c2bd55dd53b9
Amir-Galin <102375218+Amir-Galin@users.noreply.github.com>
2023-03-13 16:22:53 +0300

    Add files via upload

---
+++ prezo/chat_A.py
@@ -0,0 +1,21 @@
+from articulation import click_face, speak_and_articulate
+
+await key("click")
+slide("eyes/глаза_нейтральный1.svg")
+await speak_and_articulate("привет, ты видел, что вчера написали в группу?")
+
+await key("click")
+slide("eyes/глаза_нейтральный1.svg")
+await speak_and_articulate("зачета завтра не будет, его перенесли на субботу!")
+ 
+await key("click")
+slide("eyes/глаза_нейтральный1.svg")
+await speak_and_articulate("ты всегда так говоришь и сдаешь на тройку, может пора начинать учить предмет?")
+
+await key("click")
+slide("eyes/глаза_нейтральный1.svg")
+await speak_and_articulate("хорошо, извини. Больше не буду.")
+
+await key("click")
+slide("eyes/глаза_нейтральный1.svg")
+await speak_and_articulate("ну раз ты платишь, то я согласна!")
---
+++ prezo/chat_F.py
@@ -0,0 +1,17 @@
+from articulation import click_face, speak_and_articulate
+ 
+await key("click")
+slide("eyes/глаза_нейтральный1.svg")
+await speak_and_articulate("привет. Нет, а что случилось?")
+
+await key("click")
+slide("eyes/глаза_нейтральный1.svg")
+await speak_and_articulate("и это все? Я и так не собирался готовиться, там же все элементарно.")
+
+await key("click")
+slide("eyes/глаза_нейтральный1.svg")
+await speak_and_articulate("не повышай на меня голос!")
+
+await key("click")
+slide("eyes/глаза_нейтральный1.svg")
+await speak_and_articulate("пойдем лучше в столовую, я угощаю!")
