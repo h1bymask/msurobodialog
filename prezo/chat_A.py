1485877ddd0dd5445db3959ff1f015e1f4c46f40
Amir-Galin <102375218+Amir-Galin@users.noreply.github.com>
2023-03-13 17:15:19 +0300

    Add files via upload

--- prezo/chat_A.py
+++ prezo/chat_A.py
@@ -1,21 +1,21 @@
 from articulation import click_face, speak_and_articulate
 
 await key("click")
-slide("eyes/глаза_нейтральный1.svg")
-await speak_and_articulate("привет, ты видел, что вчера написали в группу?")
+slide("eyes/глаза_улыбающийся1.svg")
+await speak_and_articulate("привет, ты видел, что вчера написали в группу")
 
 await key("click")
-slide("eyes/глаза_нейтральный1.svg")
+slide("eyes/глаза_улыбающийся1.svg")
 await speak_and_articulate("зачета завтра не будет, его перенесли на субботу!")
  
 await key("click")
-slide("eyes/глаза_нейтральный1.svg")
-await speak_and_articulate("ты всегда так говоришь и сдаешь на тройку, может пора начинать учить предмет?")
+slide("eyes/глаза_злой.svg")
+await speak_and_articulate("ты всегда так говоришь и сдаешь на тройку, может пора начинать учить.wav")
 
 await key("click")
-slide("eyes/глаза_нейтральный1.svg")
-await speak_and_articulate("хорошо, извини. Больше не буду.")
+slide("eyes/глаза_грустный.svg")
+await speak_and_articulate("хорошо, извини. Больше не буду")
 
 await key("click")
-slide("eyes/глаза_нейтральный1.svg")
+slide("eyes/глаза_улыбающийся1.svg")
 await speak_and_articulate("ну раз ты платишь, то я согласна!")
