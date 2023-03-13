c4f679d3f75cd694948f698a1c720008fff74a9c
Vsevolod Kartashev <h1bymask@gmail.com>
2023-03-13 18:07:04 +0300

    Update chat_F.py

--- prezo/chat_F.py
+++ prezo/chat_F.py
@@ -1,13 +1,17 @@
 from articulation import click_face, speak_and_articulate, click_eyes
  
-click_eyes("eyes/глаза_нейтральный1.svg")
+await key("click")
+slide("eyes/глаза_нейтральный1.svg")
 await speak_and_articulate("привет. Нет, а что случилось")
 
-click_eyes("eyes/глаза_нейтральный1.svg")
+await key("click")
+slide("eyes/глаза_нейтральный1.svg")
 await speak_and_articulate("и это все Я и так не собирался готовиться, там же все элементарно")
 
-click_eyes("eyes/глаза_злой.svg")
+await key("click")
+slide("eyes/глаза_злой.svg")
 await speak_and_articulate("не повышай на меня голос!")
 
-click_eyes("eyes/глаза_улыбающийся1.svg")
+await key("click")
+slide("eyes/глаза_улыбающийся1.svg")
 await speak_and_articulate("пойдем лучше в столовую, я угощаю!")
