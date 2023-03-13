ef6a0f692eca872aff9171387f12ecfeba67958d
Amir-Galin <102375218+Amir-Galin@users.noreply.github.com>
2023-03-13 18:09:43 +0300

    Add files via upload

--- prezo/chat_A.py
+++ prezo/chat_A.py
@@ -1,17 +1,17 @@
 from articulation import click_face, speak_and_articulate, click_eyes
 
 
-click_eyes("eyes/глаза_улыбающийся1.svg")
+await click_eyes("eyes/глаза_улыбающийся1.svg")
 await speak_and_articulate("привет, ты видел, что вчера написали в группу")
 
-click_eyes("eyes/глаза_улыбающийся1.svg")
+await click_eyes("eyes/глаза_улыбающийся1.svg")
 await speak_and_articulate("зачета завтра не будет, его перенесли на субботу!")
  
-click_eyes("eyes/глаза_злой.svg")
+await click_eyes("eyes/глаза_злой.svg")
 await speak_and_articulate("ты всегда так говоришь и сдаешь на тройку, может пора начинать учить.wav")
 
-click_eyes("eyes/глаза_грустный.svg")
+await click_eyes("eyes/глаза_грустный.svg")
 await speak_and_articulate("хорошо, извини. Больше не буду")
 
-click_eyes("eyes/глаза_улыбающийся1.svg")
+await click_eyes("eyes/глаза_улыбающийся1.svg")
 await speak_and_articulate("ну раз ты платишь, то я согласна!")
