27b513f81d7ce35c4994961252b6e7d1d3306194
Vsevolod Kartashev <h1bymask@gmail.com>
2023-03-13 19:06:54 +0300

    Update chat_A.py

--- prezo/chat_A.py
+++ prezo/chat_A.py
@@ -11,7 +11,7 @@ await click_eyes("eyes/глаза_злой.svg")
 await speak_and_articulate("ты всегда так говоришь и сдаешь на тройку, может пора начинать учить")
 
 await click_eyes("eyes/глаза_грустный.svg")
-await speak_and_articulate("хорошо, извини. Больше не буду")
+await speak_and_articulate("хорошо, извини Больше не буду")
 
 await click_eyes("eyes/глаза_улыбающийся1.svg")
 await speak_and_articulate("ну раз ты платишь, то я согласна!")
